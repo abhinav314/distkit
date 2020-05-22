751e374bea8b52a6ba78f5c2b523ca0d3a831e74
abhinav314 <abhinav468@gmail.com>
2020-05-22 19:01:55 -0400

    initial commit

---
+++ distkit/__init__.py
@@ -0,0 +1,1 @@
+from .GaussianDist import Gaussian
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distkit',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distkit'],
+      zip_safe=False)
